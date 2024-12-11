9befd97d83c57a79d3092ec433d6fdf8cd9f7886
zELdYT <106864176+zELdYT@users.noreply.github.com>
2024-12-11 23:48:03 +0500

    Minor "cp437.h" update
    Added License commentary

--- includes/headers/encodings/cp437.h
+++ includes/headers/encodings/cp437.h
@@ -1,4 +1,11 @@
 
+/*
+ * Copyright (c) 2024 zELdYT
+ *
+ * Licensed under the BSD 2-Clause License.
+ * See the LICENSE file in the project root for more details.
+ */
+
 #ifndef __MASTER_CP437_INCLUDE_H__
 #define __MASTER_CP437_INCLUDE_H__
 
